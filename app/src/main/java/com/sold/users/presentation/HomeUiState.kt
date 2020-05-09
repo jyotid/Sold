0be3b08444dedd8421afe311f4f93448b96fef93
Jyoti Dubey <jyoti0801@gmail.com>
2020-05-09 22:40:51 +0530

    Add home ui state

---
+++ app/src/main/java/com/sold/users/presentation/HomeUiState.kt
@@ -0,0 +1,28 @@
+package com.sold.users.presentation
+
+import androidx.annotation.DrawableRes
+import com.sold.R
+
+data class HomeUiState(
+    val salutation : String,
+    @DrawableRes val image : Int = R.drawable.ic_user,
+    val categories : List<Category>,
+    val tasks : List<Task>,
+    val onCategoryClicked : ()-> Unit,
+    val onAddTaskToCategory: ()->Unit,
+    val onAddNewTask: ()->Unit
+)
+
+data class Category(
+    val name :String,
+    val taskCount : String,
+    val backgroundColor : String,
+    @DrawableRes val image: Int
+)
+
+data class Task(
+    @DrawableRes val icon : Int,
+    val title : String,
+    val completedCount : String,
+    val totalCount : String
+)
--- app/src/main/java/com/sold/users/presentation/UserViewModel.kt
+++ app/src/main/java/com/sold/users/presentation/UserViewModel.kt
@@ -2,26 +2,93 @@ package com.sold.users.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.sold.R
 import com.sold.users.data.model.User
 import com.sold.users.domain.FetchUsersUsecase
 
 
 class UserViewModel constructor(private val fetchUsersUsecase: FetchUsersUsecase){
-    private var users : MutableLiveData<List<User>> = MutableLiveData()
+    private var users : MutableLiveData<HomeUiState> = MutableLiveData()
 
-    fun fetchUsers(){
-
+    init {
+        updateData()
     }
 
-    fun getUsers() : LiveData<List<User>>{
+    fun getUsers() : LiveData<HomeUiState>{
         return users
     }
 
     fun updateData() {
-        val newList =  mutableListOf<User>()
-        newList.add(User("Jyoti","Android Developer"))
-        newList.add(User("Saurabh","Backend Developer"))
-        users.value = newList
+        users.value = HomeUiState(
+            salutation = getSalutation("Jyoti"),
+            image = R.drawable.ic_user,
+            categories = getCategories(),
+            tasks = getTask(),
+            onAddTaskToCategory = {
+
+            },
+            onCategoryClicked = {
+
+            },
+            onAddNewTask = {
+
+            }
+
+        )
     }
+
+    private fun getSalutation(name : String) = "Hello, $name"
+
+    private fun getCategories(): List<Category>{
+        val categories =  mutableListOf<Category>()
+        categories.add(Category(
+            name = "Office",
+            taskCount = "5 Tasks",
+            image = R.drawable.ic_office,
+            backgroundColor = "#fe3966"
+        ))
+        categories.add(Category(
+            name = "Personal",
+            taskCount = "4 Tasks",
+            image = R.drawable.ic_personal,
+            backgroundColor = "#057dff"
+
+        ))
+        categories.add(Category(
+            name = "Daily",
+            taskCount = "3 Tasks",
+            image = R.drawable.ic_daily,
+            backgroundColor = "#fea623"
+
+        ))
+        categories.add(Category(
+            name = "Learning",
+            taskCount = "2 Tasks",
+            image = R.drawable.ic_learning,
+            backgroundColor = "#9345cd"
+
+        ))
+        return categories
+    }
+
+    private fun getTask(): List<Task>{
+        val tasks = mutableListOf<Task>()
+        tasks.add(Task(
+            icon = R.drawable.ic_learning,
+            title = "Drawing",
+            completedCount = "2 Completed",
+            totalCount = "4"
+
+        ))
+        tasks.add(Task(
+            icon = R.drawable.ic_learning,
+            title = "Work manage",
+            completedCount = "2 Completed",
+            totalCount = "3"
+
+        ))
+        return tasks
+    }
+
 
 }
